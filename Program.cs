1c5a66f7209a7726df3fa14ee08995181fd699dc
Bart Paszkiewicz <bart.paszkiewicz@gmail.com>
2020-01-10 22:21:17 +0100

    działa super szybciej

--- Program.cs
+++ Program.cs
@@ -10,20 +10,20 @@ namespace ZabawaLiczbamiUpartymi
     class Program
     {
         private static int Counter;
-        private static List<long> ListaLiczb;
-        private static void FindNumberOfTimes(long input)
+        private static List<ulong> ListaLiczb;
+        private static void FindNumberOfTimes(ulong input)
         {
             Counter++;
             ListaLiczb.Add(input);
-            long wynik;
+            ulong wynik;
             string pomocniczystring = input.ToString();
             char[] pomocnicznyarray = pomocniczystring.ToCharArray();
             if (pomocnicznyarray.Length == 1)
                 return;
-            wynik = Convert.ToInt64(pomocnicznyarray[0].ToString());
+            wynik = Convert.ToUInt64(pomocnicznyarray[0].ToString());
             for (int i = 1; i < pomocnicznyarray.Length; i++)
             {
-                wynik *= Convert.ToInt64(pomocnicznyarray[i].ToString());
+                wynik *= Convert.ToUInt64(pomocnicznyarray[i].ToString());
             }
             if (wynik < 10)
                 return;
@@ -32,15 +32,24 @@ namespace ZabawaLiczbamiUpartymi
         }
         static void Main()
         {
+            Console.WriteLine(ulong.MaxValue.ToString());
+            Console.WriteLine(Int64.MaxValue.ToString());
             DataTable dt = new DataTable();
             dt.Columns.Add("NumberOfMultiplication", typeof(int));
-            dt.Columns.Add("ListOfSteps", typeof(List<long>));
-            dt.Rows.Add(0, new List<long>());
+            dt.Columns.Add("ListOfSteps", typeof(List<ulong>));
+            dt.Rows.Add(0, new List<ulong>());
 
-            for (long i = 12729788311; i<Int64.MaxValue; i++)
+            ulong i = 10;
+            while(i <= UInt64.MaxValue)// (ulong i = 10; i<Int64.MaxValue; i++)
             {
-                ListaLiczb = new List<long>();
+                //Console.WriteLine(i);
+
+                ListaLiczb = new List<ulong>();
                 Counter = 0;
+                
+                
+
+
                 FindNumberOfTimes(i);
                 if ((int)dt.Rows[0]["NumberOfMultiplication"] < Counter)
                 {
@@ -48,8 +57,54 @@ namespace ZabawaLiczbamiUpartymi
                     dt.Rows[0]["ListOfSteps"] = ListaLiczb;
                     Console.WriteLine(i + " | " + Counter);
                 }
+
+                char[] temp = i.ToString().ToCharArray();
+                bool charfound = false;
+                for (int j =1; j<temp.Length; j++)
+                {
+                    if(!(Convert.ToInt16(temp[j-1].ToString()) <= Convert.ToInt16(temp[j].ToString())))
+                    {
+                        ulong temp1 = Convert.ToUInt64((Convert.ToInt16(temp[j - 1].ToString()) - Convert.ToInt16(temp[j].ToString())) * Math.Pow(10.0, temp.Length - j - 1));
+                        if (temp1 + i < UInt64.MaxValue)
+                        {
+                            i += temp1;
+                            charfound = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is the end. i = {0}", i);
+                            Console.ReadKey();
+                            return;
+                        }
+                        //break;
+                    }
+                }
+                if (i.ToString().Contains("0"))
+                {
+                    if (!UInt64.TryParse(i.ToString().Replace("0", "2"), out i))
+                    {
+                        Console.WriteLine("This is the end. i = {0}", i);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else if (!charfound)
+                {
+                    if (i + 1 <= UInt64.MaxValue)
+                        i++;
+                    else
+                    {
+                        Console.WriteLine("This is the end. i = {0}", i);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+
+
             }
-            
+            Console.WriteLine("This is the end. i = {0}", i);
+            Console.ReadKey();
+            return;
             
         }
     }
